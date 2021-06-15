d81247cca754a2f346f61d54e9d5a40404297a9d
Wojciech Jarosz <jaroszw@gmail.com>
2021-06-15 20:57:39 +0200

    schema implemented

--- server/index.js
+++ server/index.js
@@ -1,57 +1,64 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require('apollo-server');
+const { mainCards, animals, categories } = require('./db');
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
+  type MainCard {
+    title: String!
+    image: String!
   }
 
-  type MainCard {
-    title: String
-    image: String
+  type Animal {
+    id: ID!
+    image: String!
+    title: String!
+    rating: Float
+    price: String!
+    description: [String!]!
+    slug: String!
+    stock: Int!
+    onSale: Boolean
+    category: Category
+  }
+
+  type Category {
+    id: ID!
+    image: String!
+    category: String!
+    slug: String!
+    animals: [Animal!]!
   }
 
   type Query {
-    books: [Book]
     mainCards: [MainCard]
+    animals: [Animal!]!
+    animal(slug: String!): Animal
+    categories: [Category!]!
+    category(slug: String!): Category
   }
 `;
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-const mainCards = [
-  {
-    title: "Recently reviewed",
-    image: "lion",
-  },
-  {
-    title: "Lookiong for a gift",
-    image: "pinguin",
-  },
-  {
-    title: "Best behived",
-    image: "cat",
-  },
-];
-
 const resolvers = {
   Query: {
-    books: () => books,
     mainCards: () => mainCards,
+    animals: () => animals,
+    categories: () => categories,
+    animal: (parent, args, ctx) =>
+      animals.find((animal) => animal.slug === args.slug),
+    category: (parent, args, ctx) =>
+      categories.find((category) => category.slug === args.slug),
+  },
+  Category: {
+    animals: (parent, args, ctx) =>
+      animals.filter((animal) => animal.category === parent.id),
+  },
+  Animal: {
+    category: (parent, args, ctx) =>
+      categories.find((category) => category.id === parent.category),
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`Server ready at ${url}`);
 });
