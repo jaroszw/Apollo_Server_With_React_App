5a106979208d97ff1552829c8a9f5cf73255cc59
Wojciech Jarosz <jaroszw@gmail.com>
2021-06-16 20:17:00 +0200

    final

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,8 +1,8 @@
-import React from "react";
-import MainHero from "../components/MainHero/MainHero";
-import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay";
-import CardDisplay from "../components/CardDisplay/CardDisplay";
-import { useQuery, gql } from "@apollo/client";
+import React, { useEffect } from 'react';
+import MainHero from '../components/MainHero/MainHero';
+import CategoryDisplay from '../components/CategoryDisplay/CategoryDisplay';
+import CardDisplay from '../components/CardDisplay/CardDisplay';
+import { useQuery, gql, useMutation } from '@apollo/client';
 
 const FETCH_MAIN_CARDS = gql`
   {
@@ -16,8 +16,40 @@ const FETCH_MAIN_CARDS = gql`
   }
 `;
 
+const ADD_ANIMAL_MUTATION = gql`
+  mutation (
+    $image: String!
+    $category: String!
+    $title: String!
+    $stock: Int!
+    $price: String!
+    $description: [String!]!
+    $rating: Float
+    $slug: String!
+  ) {
+    addAnimal(
+      image: $image
+      category: $category
+      title: $title
+      stock: $stock
+      price: $price
+      description: $description
+      rating: $rating
+      slug: $slug
+    ) {
+      id
+    }
+  }
+`;
+
 function LandingPage() {
-  const { data, loading, error } = useQuery(FETCH_MAIN_CARDS);
+  const {
+    data: { animals },
+    loading,
+    error,
+  } = useQuery(FETCH_MAIN_CARDS);
+
+  const [addAnimal] = useMutation(ADD_ANIMAL_MUTATION);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error...</div>;
@@ -26,7 +58,29 @@ function LandingPage() {
     <div>
       <MainHero />
       <CategoryDisplay />
-      <CardDisplay animals={data.animals} />
+      <CardDisplay animals={animals} />
+      <button
+        onClick={() =>
+          addAnimal({
+            variables: {
+              image: 'ostrich',
+              category: '1',
+              title: 'This is a reeally cool ostrich',
+              stock: 13,
+              price: '32.22',
+              description: [
+                'Nice ostrich',
+                'What the fuck is ostrich',
+                'Wana buty ostrich buy ostrich',
+              ],
+              rating: 3.5,
+              slug: 'ostrich',
+            },
+          })
+        }
+      >
+        Add an ostritch
+      </button>
     </div>
   );
 }
