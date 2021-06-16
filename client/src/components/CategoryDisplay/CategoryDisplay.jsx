60bdbdb8f86c35025a461ecb17a1170a2312df07
Wojciech Jarosz <jaroszw@gmail.com>
2021-06-16 15:16:39 +0200

    fetch data with variables

--- client/src/components/CategoryDisplay/CategoryDisplay.jsx
+++ client/src/components/CategoryDisplay/CategoryDisplay.jsx
@@ -1,29 +1,49 @@
-import React, { useState } from 'react'
-import "./CategoryDisplay.css"
-import { Container } from 'react-bootstrap'
-import animals from '../../assets/images'
+import React, { useState } from "react";
+import "./CategoryDisplay.css";
+import { Container } from "react-bootstrap";
+import animals from "../../assets/images";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
+import { useQuery, gql } from "@apollo/client";
+
+const FETCH_CATEGORIES = gql`
+  {
+    categories {
+      id
+      image
+      category
+      slug
+    }
+  }
+`;
 
 function CategoryDisplay() {
+  const { loading, error, data } = useQuery(FETCH_CATEGORIES);
 
-    return (
-        <div className="CategoryDisplay">
-            <Container className="CategoryDisplay-container">
-                {[].map(category => {
-                    return (
-                        <Link to={`/products/${category.slug}`} className="CategoryDisplay-card-container">
-                            <div className="CategoryDisplay-card">
-                                <img src={animals[category.img]} /> 
-                            </div>
-                            <h3>{category.category}</h3>
-                        </Link>
-                    )
-                })}
-            </Container>
-        </div>
-    )
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>Error...</div>;
+
+  return (
+    <div className="CategoryDisplay">
+      <Container className="CategoryDisplay-container">
+        {data.categories.map((category) => {
+          return (
+            <Link
+              to={`/products/${category.slug}`}
+              className="CategoryDisplay-card-container"
+            >
+              <div className="CategoryDisplay-card">
+                <img src={animals[category.image]} />
+              </div>
+              <h3>{category.category}</h3>
+            </Link>
+          );
+        })}
+      </Container>
+    </div>
+  );
 }
 
-export default CategoryDisplay
+export default CategoryDisplay;
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import MainHero from "../components/MainHero/MainHero"
-import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
-import CardDisplay from "../components/CardDisplay/CardDisplay"
+import React from "react";
+import MainHero from "../components/MainHero/MainHero";
+import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay";
+import CardDisplay from "../components/CardDisplay/CardDisplay";
+import { useQuery, gql } from "@apollo/client";
+
+const FETCH_MAIN_CARDS = gql`
+  {
+    animals {
+      id
+      image
+      price
+      slug
+      title
+    }
+  }
+`;
 
 function LandingPage() {
+  const { data, loading, error } = useQuery(FETCH_MAIN_CARDS);
 
-    return (
-        <div>
-            <MainHero />
-            <CategoryDisplay />
-            <CardDisplay animals={[]}/>
-        </div>
-    )
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error...</div>;
+
+  return (
+    <div>
+      <MainHero />
+      <CategoryDisplay />
+      <CardDisplay animals={data.animals} />
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
